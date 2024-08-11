6966137dbff1652501e10933d0bb796432206870
Jirachai C <jirachai.c@outlook.com>
2024-08-11 19:32:54 +0800

    Update build

--- scripts/add-tampermonkey-header.sh
+++ scripts/add-tampermonkey-header.sh
@@ -8,6 +8,8 @@ echo -e "// ==UserScript==
 // @author       Jirachai Chansivanon
 // @match        https://www.hareshi.net/browse/anime*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hareshi.net
+// @downloadURL  https://raw.githubusercontent.com/antronic/hareshi-my-list/main/build/hareshi-my-list.tampermonkey.min.js
+// @version      0.1
 // @grant        none
 // ==/UserScript==
 \n$(cat build/hareshi-my-list.min.js)

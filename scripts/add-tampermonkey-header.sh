fb5888d87f345642332bb23fccc75254fd3726ce
Jirachai C <jirachai.c@outlook.com>
2024-08-11 19:35:50 +0800

    Update script

--- scripts/add-tampermonkey-header.sh
+++ scripts/add-tampermonkey-header.sh
@@ -2,11 +2,11 @@
 
 echo -e "// ==UserScript==
 // @name         Hareshi - My List
+// @match        https://www.hareshi.net/browse/anime*
+// @description  Hareshi's Saved list
 // @namespace    jirachai.me
 // @version      $(date '+%Y-%m-%d')
-// @description  Hareshi is an anime directory website that provides a comprehensive list of anime and channels to watch them. However, it doesn’t include a “favorite” or "saved list" feature, which is why I built this script.
 // @author       Jirachai Chansivanon
-// @match        https://www.hareshi.net/browse/anime*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hareshi.net
 // @downloadURL  https://raw.githubusercontent.com/antronic/hareshi-my-list/main/build/hareshi-my-list.tampermonkey.min.js
 // @version      0.1

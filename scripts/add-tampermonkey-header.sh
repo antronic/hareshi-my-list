7ae64ffb53090cb7588110be440da7b0fab5272d
Jirachai C <jirachai.c@outlook.com>
2024-08-11 20:24:40 +0800

    Update script to match all hareshi website

--- scripts/add-tampermonkey-header.sh
+++ scripts/add-tampermonkey-header.sh
@@ -2,14 +2,14 @@
 
 echo -e "// ==UserScript==
 // @name         Hareshi - My List
-// @match        https://www.hareshi.net/browse/anime*
+// @match        https://www.hareshi.net/*
 // @description  Hareshi's Saved list
 // @namespace    jirachai.me
 // @version      $(date '+%Y-%m-%d')
 // @author       Jirachai Chansivanon
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hareshi.net
 // @downloadURL  https://raw.githubusercontent.com/antronic/hareshi-my-list/main/build/hareshi-my-list.tampermonkey.min.js
-// @version      0.1
+// @version      0.2
 // @grant        none
 // ==/UserScript==
 \n$(cat build/hareshi-my-list.min.js)
